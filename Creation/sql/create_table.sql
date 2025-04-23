abd85666755da7c12d701b4a39e27ec2f16ae31e
Guillaume <guillaumexue@outlook.fr>
2025-04-23 18:30:36 +0200

    Refactor database schema: update user, session, ticket, and event tables; add new relationships and constraints

--- Creation/sql/create_table.sql
+++ Creation/sql/create_table.sql
@@ -1,123 +1,119 @@
 CREATE TABLE Utilisateur (
-    id_utilisateur SERIAL PRIMARY KEY,
-    nom VARCHAR(255) NOT NULL,
-    prenom VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL
-);
-
-CREATE TABLE StatutUtilisateur (
-    id_statut SERIAL PRIMARY KEY,
-    description TEXT NOT NULL,
-    date_expiration DATE
-);
-
-CREATE TABLE Avantage (
-    id_avantage SERIAL PRIMARY KEY,
-    description TEXT NOT NULL,
-    date_expiration DATE
-);
-
-CREATE TABLE HistoriqueTransaction (
-    id_transaction SERIAL PRIMARY KEY,
-    action VARCHAR(255) NOT NULL,
-    attente BOOLEAN NOT NULL,
-    fait BOOLEAN NOT NULL
+    Uemail VARCHAR(255) UNIQUE NOT NULL,
+    Unom VARCHAR(64) NOT NULL,
+    Uprenom VARCHAR(64) NOT NULL,
+    Ustatut VARCHAR(255) NOT NULL,
+    Unb_max_billets INT NOT NULL,
+    PRIMARY KEY (Uemail),
+    CHECK (Ustatut IN ('Normal', 'VIP', 'VVIP')),
+    CHECK (Unb_max_billets > 0)
 );
 
 CREATE TABLE CreneauConnexion (
-    id_creneau SERIAL PRIMARY KEY,
-    date TIMESTAMP NOT NULL
-);
-
-CREATE TABLE PreReservation (
-    id_prereservation SERIAL PRIMARY KEY,
-    date TIMESTAMP NOT NULL
+    CCdate_heure_debut TIMESTAMP NOT NULL,
+    CCetat VARCHAR(255) NOT NULL,
+    PRIMARY KEY (CCdate_heure_debut)
 );
 
 CREATE TABLE Billet (
-    id_billet SERIAL PRIMARY KEY,
-    date_achat TIMESTAMP NOT NULL
+    Bid TEXT NOT NULL,
+    Bprix_final DECIMAL(10, 2) NOT NULL,
+    Bpromotion DECIMAL(10, 2) NOT NULL,
+    PRIMARY KEY (Bid),
 );
 
 CREATE TABLE PolitiqueEtablissement (
-    id_politique SERIAL PRIMARY KEY,
+    PEtitre VARCHAR(255) NOT NULL,
     description TEXT NOT NULL
 );
 
 CREATE TABLE Etablissement (
-    id_etablissement SERIAL PRIMARY KEY,
-    nom VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE Seance (
-    id_seance SERIAL PRIMARY KEY,
-    date TIMESTAMP NOT NULL,
-    id_etablissement INT NOT NULL,
-    FOREIGN KEY (id_etablissement) REFERENCES Etablissement(id_etablissement)
+    ETAadresse VARCHAR(255) NOT NULL,
+    ETAnom VARCHAR(255) NOT NULL,
+    PRIMARY KEY (ETAadresse)
 );
 
 CREATE TABLE Categorie (
-    id_categorie SERIAL PRIMARY KEY,
-    nom VARCHAR(255) NOT NULL
+    CATnom VARCHAR(255) NOT NULL,
+    CATprix DECIMAL(10, 2) NOT NULL,
+    CATnum_place INT NOT NULL,
+    PRIMARY KEY (Cnom)
 );
 
 CREATE TABLE Evenement (
-    id_evenement SERIAL PRIMARY KEY,
-    nom VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE Film (
-    id_film SERIAL PRIMARY KEY,
-    titre VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE Concert (
-    id_concert SERIAL PRIMARY KEY,
-    artiste VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE SousEvenement (
-    id_sous_evenement SERIAL PRIMARY KEY,
-    nom VARCHAR(255) NOT NULL
+    Eid TEXT NOT NULL,
+    Enom VARCHAR(255) NOT NULL,
+    Edate TIMESTAMP NOT NULL,
+    Enum_salle INT NOT NULL,
+    Edescription TEXT NOT NULL,
+    Etype VARCHAR(32) NOT NULL,
+    PRIMARY KEY (Eid)
+    CHECK (Etype IN ('Concert', 'SousEvenement', 'Film'))
 );
 
 -- Relations
-CREATE TABLE UtilisateurStatut (
-    id_utilisateur INT NOT NULL,
-    id_statut INT NOT NULL,
-    PRIMARY KEY (id_utilisateur, id_statut),
-    FOREIGN KEY (id_utilisateur) REFERENCES Utilisateur(id_utilisateur),
-    FOREIGN KEY (id_statut) REFERENCES StatutUtilisateur(id_statut)
+CREATE TABLE Echange (
+    Uemail_emetteur VARCHAR(255) NOT NULL,
+    Uemail_destinataire VARCHAR(255) NOT NULL,
+    Edate TIMESTAMP NOT NULL,
+    FOREIGN KEY (Uemail_emetteur) REFERENCES Utilisateur(Uemail),
+    FOREIGN KEY (Uemail_destinataire) REFERENCES Utilisateur(Uemail),
+    PRIMARY KEY (Uemail_emetteur, Uemail_destinataire, Edate)
+)
+
+CREATE TABLE Achat (
+    Uemail VARCHAR(255) NOT NULL,
+    Bid TEXT NOT NULL,
+    Adate TIMESTAMP NOT NULL,
+    FOREIGN KEY (Uemail) REFERENCES Utilisateur(Uemail),
+    FOREIGN KEY (Bid) REFERENCES Billet(Bid),
+    PRIMARY KEY (Uemail, Bid)
 );
 
-CREATE TABLE UtilisateurAvantage (
-    id_utilisateur INT NOT NULL,
-    id_avantage INT NOT NULL,
-    PRIMARY KEY (id_utilisateur, id_avantage),
-    FOREIGN KEY (id_utilisateur) REFERENCES Utilisateur(id_utilisateur),
-    FOREIGN KEY (id_avantage) REFERENCES Avantage(id_avantage)
+CREATE TABLE CreneauConnexionBillet (
+    CCdate_heure_debut TIMESTAMP NOT NULL,
+    Bid TEXT NOT NULL,
+    FOREIGN KEY (CCdate_heure_debut) REFERENCES CreneauConnexion(CCdate_heure_debut),
+    FOREIGN KEY (Bid) REFERENCES Billet(Bid),
+    PRIMARY KEY (CCdate_heure_debut, Bid)
 );
 
-CREATE TABLE BilletSeance (
-    id_billet INT NOT NULL,
-    id_seance INT NOT NULL,
-    PRIMARY KEY (id_billet, id_seance),
-    FOREIGN KEY (id_billet) REFERENCES Billet(id_billet),
-    FOREIGN KEY (id_seance) REFERENCES Seance(id_seance)
+CREATE TABLE CreneauConnexionUtilisateur (
+    CCdate_heure_debut TIMESTAMP NOT NULL,
+    Uemail VARCHAR(255) NOT NULL,
+    FOREIGN KEY (CCdate_heure_debut) REFERENCES CreneauConnexion(CCdate_heure_debut),
+    FOREIGN KEY (Uemail) REFERENCES Utilisateur(Uemail),
+    PRIMARY KEY (CCdate_heure_debut, Uemail)
 );
 
-CREATE TABLE SeanceCategorie (
-    id_seance INT NOT NULL,
-    id_categorie INT NOT NULL,
-    PRIMARY KEY (id_seance, id_categorie),
-    FOREIGN KEY (id_seance) REFERENCES Seance(id_seance),
-    FOREIGN KEY (id_categorie) REFERENCES Categorie(id_categorie)
+CREATE TABLE EtablissementPolitique (
+    ETAadresse VARCHAR(255) NOT NULL,
+    PEtitre VARCHAR(255) NOT NULL,
+    FOREIGN KEY (ETAadresse) REFERENCES Etablissement(ETAadresse),
+    FOREIGN KEY (PEtitre) REFERENCES PolitiqueEtablissement(PEtitre),
+    PRIMARY KEY (ETAadresse, PEtitre)
 );
 
-CREATE TABLE EvenementSousEvenement (
-    id_evenement INT NOT NULL,
-    id_sous_evenement INT NOT NULL,
-    PRIMARY KEY (id_evenement, id_sous_evenement),
-    FOREIGN KEY (id_evenement) REFERENCES Evenement(id_evenement),
-    FOREIGN KEY (id_sous_evenement) REFERENCES SousEvenement(id_sous_evenement)
+CREATE TABLE CategorieBillet (
+    CATnom VARCHAR(255) NOT NULL,
+    Bid TEXT NOT NULL,
+    FOREIGN KEY (CATnom) REFERENCES Categorie(CATnom),
+    FOREIGN KEY (Bid) REFERENCES Billet(Bid),
+    PRIMARY KEY (CATnom, Bid)
+);
+
+CREATE TABLE CategorieEvenement (
+    CATnom VARCHAR(255) NOT NULL,
+    Eid TEXT NOT NULL,
+    FOREIGN KEY (CATnom) REFERENCES Categorie(CATnom),
+    FOREIGN KEY (Eid) REFERENCES Evenement(Eid),
+    PRIMARY KEY (CATnom, Eid)
+);
+
+CREATE TABLE EvenementEtablissement (
+    Eid TEXT NOT NULL,
+    ETAadresse VARCHAR(255) NOT NULL,
+    FOREIGN KEY (Eid) REFERENCES Evenement(Eid),
+    FOREIGN KEY (ETAadresse) REFERENCES Etablissement(ETAadresse),
+    PRIMARY KEY (Eid, ETAadresse)
 );
